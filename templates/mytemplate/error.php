17d8c3a7dae26ad74bf724cb28eba8d7c86d7d92
panickylemon <alenka__1992@mail.ru>
2016-03-09 22:11:26 +0300

    bg

---
+++ templates/mytemplate/error.php
@@ -0,0 +1,7 @@
+<?php
+defined( '_JEXEC' ) or die( 'Restricted access' );
+?>
+<?php
+header("HTTP/1.1 404 Not Found");
+echo file_get_contents(JURI::root().'/index.php?option=com_content&amp;view=article&amp;id=15');
+?>
