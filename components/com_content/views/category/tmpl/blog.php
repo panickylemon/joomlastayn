f86b62d941fcbbe6c62ac7635658b41bf2146cbf
panickylemon <alenka__1992@mail.ru>
2016-03-10 18:29:35 +0300

    error

--- components/com_content/views/category/tmpl/blog.php
+++ components/com_content/views/category/tmpl/blog.php
@@ -13,9 +13,6 @@ JHtml::addIncludePath(JPATH_COMPONENT . '/helpers');
 
 JHtml::_('behavior.caption');
 ?>
-<div class="gg">
-
-</div>
 
 <div class="blog<?php echo $this->pageclass_sfx; ?>" itemscope itemtype="http://schema.org/Blog">
 	<?php if ($this->params->get('show_page_heading')) : ?>
