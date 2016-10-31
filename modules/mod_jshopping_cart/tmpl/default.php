ec655296f956d857faec8ee2eb798a62539efa00
panickylemon <alenka__1992@mail.ru>
2016-10-31 22:47:09 +0300

    error

--- modules/mod_jshopping_cart/tmpl/default.php
+++ modules/mod_jshopping_cart/tmpl/default.php
@@ -4,7 +4,7 @@
     ?>">
         <img class="left" src="/joomlastayn/templates/mytemplate/images/cart2.png" alt="корзина">
         <span class="cart_header_text">Корзина</span>
-        <span class="count_cart_header right" id = "jshop_quantity_products">(<?php print
+        <span class="count_cart_header" id = "jshop_quantity_products">(<?php print
                 $cart->count_product?>)
         </span>
     </a>
